4e1138de53a4a64aa45d3dff410afa242efd89af
hugh.hu <hugh.hu@fredfarid.com>
2015-12-22 12:22:55 +0800

    test

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 var express = require('express');
-var app = express();
-app.use(require('body-parser')());
+var bodyParser = require('body-parser');
+var path =require('path');
 
+var app = express();
+
+app.use(bodyParser());
 
 app.set('port', process.env.PORT || 3000);
+
+//app.use(express.static('../client/'));
+
+app.set('views', 'client/htmls');
 
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.' );
 });
 
-
 app.post('/test', function(req, res){
 
     res.type('text/plain');
     res.send('Mobile Num:' + req.body.mobileNum);
+});
+
+app.get('/html', function(req, res){
+
+    res.sendFile(path.join(__dirname, '/client/htmls/test.html'));
 });
 
 // custom 404 page
