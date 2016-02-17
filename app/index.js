e0330ec96ebbbb7f47cead45332c4bb4356e64b9
hugh.hu <hugh.hu@fredfarid.com>
2016-02-17 10:40:29 +0800

    test

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var generators = require('yeoman-generator');
 var Base = generators.Base;
 var path = require('path');
+var bd;
 
 module.exports = Base.extend({
 
@@ -8,6 +9,25 @@ module.exports = Base.extend({
 
         Base.apply(this, arguments);
         this.argument('p', {type: String, required: false});
+    },
+
+    prompting: function () {
+
+        this.spawnCommandSync('npm', ['init']);
+        this.spawnCommandSync('bower', ['init']);
+        var cb = this.async();
+        this.prompt({
+            type: "checkbox",
+            name: 'bowerDependencies',
+            message: 'What do you want?',
+            choices: ['foundation-sites#6.1.1', 'angular#1.3.15', 'lodash#3.10.1', 'moment#2.11.0',
+                'EaselJS#0.8.2', 'PreloadJS#0.6.2', 'SoundJS#0.6.2', 'TweenJS#0.6.2',
+                'gsap#1.18.2', 'owlcarousel#1.3.2'],
+            default: ['foundation-sites#6.1.1', 'angular#1.3.15', 'lodash#3.10.1']
+        }, function (a) {
+            bd = a.bowerDependencies;
+            cb();
+        }.bind(this));
     },
 
     writing: function () {
@@ -24,11 +44,11 @@ module.exports = Base.extend({
             this.spawnCommandSync('mkdir', ['htmls', 'images', 'js', 'maps', 'scripts', 'stylesheets']);
         }
         this.spawnCommandSync('touch', ['README.md']);
+
     },
 
     install: function () {
 
-        //this.spawnCommand('npm', ['install'], {'saveDev': true});
         var p = this.p;
         if (p && !('c' === p || 'f' === p)) {
             return;
@@ -44,17 +64,23 @@ module.exports = Base.extend({
                 'gulp-uglify@1.1.0', 'gulp-ruby-sass@1.3.0',
                 'browser-sync@2.8.2'], {'saveDev': true}, function () {
             });
+
+            this.bowerInstall(bd, {'save': true}, function () {
+                this.spawnCommandSync('mv', ['-f', 'bower.json', 'bower_components', 'client']);
+            }.bind(this));
         } else if ('c' === p) {
             this.npmInstall(['bower@1.7.1', 'gulp@3.8.11',
                 'gulp-concat@2.5.2', 'gulp-sourcemaps@1.5.0',
                 'gulp-uglify@1.1.0', 'gulp-ruby-sass@1.3.0',
                 'browser-sync@2.8.2'], {'saveDev': true}, function () {
             });
-            //this.bowerInstall(['foundation-sites#6.1.1',
-            //    'EaselJS#0.8.2', 'PreloadJS#0.6.2', 'SoundJS#0.6.2', 'TweenJS#0.6.2',
-            //    'lodash#3.10.1', 'moment#2.11.0', 'gsap#1.18.2', 'angular#1.3.15'
-            //], {'save': true}, function () {
-            //})
+
+            this.bowerInstall(bd, {'save': true}, function () {
+                this.spawnCommandSync('mv', ['-f', 'bower.json', 'bower_components', 'client']);
+            }.bind(this));
         }
+    },
+
+    done: function () {
     }
 });
