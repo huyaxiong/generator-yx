cf72d62f7e25bc5f64a8860333a540ae450e8e4a
hugh.hu <hugh.hu@fredfarid.com>
2016-03-07 16:12:56 +0800

    test

--- app/index.js
+++ app/index.js
@@ -64,7 +64,7 @@ module.exports = Base.extend({
             message: 'What do you want?',
             choices: ['foundation-sites#6.2.0', 'susy#2.2.12', 'angular#1.3.15',
                 'EaselJS#0.8.2', 'PreloadJS#0.6.2', 'SoundJS#0.6.2', 'TweenJS#0.6.2',
-                'gsap#1.18.2', 'moment#2.11.0', 'owlcarousel#1.3.2'],
+                'gsap#1.18.2', 'moment#2.11.0', 'owlcarousel#1.3.2', 'hammer.js#2.0.6'],
             default: ['foundation-sites#6.2.0', 'susy#2.2.12']
         }, function (a) {
             bowerDependencies = a.bowerDependencies;
@@ -98,7 +98,7 @@ module.exports = Base.extend({
         if (p && !('c' === p || 'f' === p)) {
             return;
         } else if (!p || 'f' === p) {
-            this.npmInstall(['express@4.13.3', 'body-parser@1.14.2',
+            this.npmInstall(['express@4.13.3', 'body-parser@1.14.2', 'request@2.69.0',
                 'mongoose@4.4.4'], {'save': true}, function () {
             });
 
