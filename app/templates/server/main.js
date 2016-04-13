904f054679b02d024c008730e9a5170e36f57fd1
hugh.hu <hugh.hu@fredfarid.com>
2016-04-13 18:27:25 +0800

    test

--- app/templates/server/main.js
+++ app/templates/server/main.js
@@ -1,8 +1,8 @@
-require('config/db')();
+require('./config/db')();
 
-var app = require('config/router')();
-require('routers/test')(app);
-require('config/error')(app);
+var app = require('./config/router')();
+require('./routers/test')(app);
+require('./config/error')(app);
 
 
 
--- app/templates/server/models/User.js
+++ app/templates/server/models/User.js
@@ -5,6 +5,6 @@ var schema = mongoose.Schema({
     name: String,
     mobile: String
 });
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', schema);
 
 module.exports = User;
