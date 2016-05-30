757ec56f3b79561fba05a730019a784524b0b524
hugh.hu <hugh.hu@fredfarid.com>
2016-05-30 16:51:19 +0800

    test

--- app/index.js
+++ app/index.js
@@ -54,17 +54,19 @@ module.exports = Base.extend({
     },
 
     prompting: function () {
-
+        
+        var bowerDeps = ['jquery#2.2.3', 'susy#2.2.12', 'foundation-sites#6.2.1', 'angular#1.4.9',
+            'ui-route#0.2.18', 'angular-resource#1.4.9',
+            'angular-animate#1.4.9', 'oclazyload#1.0.9',
+            'EaselJS#0.8.2', 'PreloadJS#0.6.2',
+            'gsap#1.18.2', 'hammer.js#2.0.6'];
         var cb = this.async();
+        
         this.prompt({
             type: "checkbox",
             name: 'bowerDependencies',
             message: 'What do you want?',
-            choices: ['jquery#2.2.3', 'susy#2.2.12', 'foundation-sites#6.2.1', 'angular#1.4.9',
-                'ui-route#0.2.18', 'angular-resource#1.4.9',
-                'angular-animate#1.4.9', 'oclazyload#1.0.9',
-                'EaselJS#0.8.2', 'PreloadJS#0.6.2',
-                'gsap#1.18.2', 'hammer.js#2.0.6'],
+            choices: bowerDeps,
             default: []
         }, function (a) {
             bowerDependencies = a.bowerDependencies;
@@ -95,31 +97,29 @@ module.exports = Base.extend({
     install: function () {
 
         var p = this.p;
+        var npmDeps = ['express@4.13.3', 'body-parser@1.14.2', 'compression@1.6.1', 'request@2.69.0',
+            'mongoose@4.4.4', 'node-uuid@1.4.7', 'moment@2.12.0', 'multer@1.1.0'];
+        var npmDevDeps = ['bower@1.7.1', 'gulp@3.8.11',
+            'gulp-babel@6.1.2', 'babel-preset-es2015@6.6.0',
+            'gulp-concat@2.5.2', 'gulp-sourcemaps@1.5.0',
+            'gulp-uglify@1.1.0', 'gulp-postcss@6.1.0',
+            'autoprefixer@6.3.3', 'postcss-scss@0.1.6', 'postcss-pxtorem@3.3.1',
+            'gulp-sass@2.2.0', 'browser-sync@2.8.2'];
+        
         if (p && !('c' === p || 'f' === p)) {
             return;
         } else if (!p || 'f' === p) {
-            this.npmInstall(['express@4.13.3', 'body-parser@1.14.2', 'compression@1.6.1', 'request@2.69.0',
-                'mongoose@4.4.4', 'node-uuid@1.4.7', 'moment@2.12.0'], {'save': true}, function () {
+            this.npmInstall(npmDeps, {'save': true}, function () {
             });
 
-            this.npmInstall(['bower@1.7.1', 'gulp@3.8.11',
-                'gulp-babel@6.1.2', 'babel-preset-es2015@6.6.0',
-                'gulp-concat@2.5.2', 'gulp-sourcemaps@1.5.0',
-                'gulp-uglify@1.1.0', 'gulp-postcss@6.1.0',
-                'autoprefixer@6.3.3', 'postcss-scss@0.1.6', 'postcss-pxtorem@3.3.1',
-                'gulp-sass@2.2.0', 'browser-sync@2.8.2'], {'saveDev': true}, function () {
+            this.npmInstall(npmDevDeps, {'saveDev': true}, function () {
             });
 
             this.bowerInstall(bowerDependencies, {'save': true}, function () {
                 this.spawnCommandSync('mv', ['-f', 'bower.json', 'bower_components', 'client']);
             }.bind(this));
         } else if ('c' === p) {
-            this.npmInstall(['bower@1.7.1', 'gulp@3.8.11',
-                'gulp-babel@6.1.2', 'babel-preset-es2015@6.6.0',
-                'gulp-concat@2.5.2', 'gulp-sourcemaps@1.5.0',
-                'gulp-uglify@1.1.0', 'gulp-postcss@6.1.0',
-                'autoprefixer@6.3.3', 'postcss-scss@0.1.6', 'postcss-pxtorem@3.3.1',
-                'gulp-sass@2.2.0', 'browser-sync@2.8.2'], {'saveDev': true}, function () {
+            this.npmInstall(npmDevDeps, {'saveDev': true}, function () {
             });
 
             this.bowerInstall(bowerDependencies, {'save': true}, function () {
