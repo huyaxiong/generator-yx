d3f22c63bfee342277555e7582e0afe28422eb03
hugh.hu <hugh.hu@fredfarid.com>
2016-06-15 17:15:29 +0800

    test

--- app/index.js
+++ app/index.js
@@ -2,6 +2,13 @@ var generators = require('yeoman-generator');
 var Base = generators.Base;
 var path = require('path');
 var bowerDependencies;
+var npmDeps = [];
+var npmDevDeps = ['gulp@3.8.11',
+    'gulp-babel@6.1.2', 'babel-preset-es2015@6.6.0',
+    'gulp-concat@2.5.2', 'gulp-sourcemaps@1.5.0',
+    'gulp-uglify@1.1.0', 'gulp-postcss@6.1.0',
+    'autoprefixer@6.3.3', 'postcss-scss@0.1.6', 'postcss-pxtorem@3.3.1',
+    'gulp-sass@2.2.0', 'browser-sync@2.8.2'];
 
 
 module.exports = Base.extend({
@@ -25,28 +32,6 @@ module.exports = Base.extend({
         this.fs.writeJSON('package.json', config);
     },
 
-    initBowerConfig: function () {
-
-        var config = {
-            "name": this.appname,
-            "description": "",
-            "main": "",
-            "authors": "",
-            "license": "ISC",
-            "moduleType": [],
-            "homepage": "",
-            "ignore": [
-                "**/.*",
-                "node_modules",
-                "bower_components",
-                "test",
-                "tests"
-            ],
-            "dependencies": {}
-        };
-        this.fs.writeJSON('bower.json', config);
-    },
-
     constructor: function () {
 
         Base.apply(this, arguments);
@@ -55,21 +40,19 @@ module.exports = Base.extend({
 
     prompting: function () {
         
-        var bowerDeps = ['jquery#2.2.3', 'susy#2.2.12', 'foundation-sites#6.2.1', 'angular#1.4.9',
-            'ui-route#0.2.18', 'angular-resource#1.4.9',
-            'angular-animate#1.4.9', 'oclazyload#1.0.9',
-            'EaselJS#0.8.2', 'PreloadJS#0.6.2',
-            'gsap#1.18.2', 'hammer.js#2.0.6'];
+        var frontendDeps = ['jquery@2.2.3', 'susy@2.2.12', 'foundation-sites@6.2.3', 'angular@1.4.9',
+            'ui-route@0.2.18', 'angular-resource@1.4.9', 'angular-animate@1.4.9',
+            'gsap@1.18.2', 'popmotion@5.0.12', 'hammer.js@2.0.6'];
         var cb = this.async();
         
         this.prompt({
             type: "checkbox",
-            name: 'bowerDependencies',
+            name: 'frontendDeps',
             message: 'What do you want?',
-            choices: bowerDeps,
+            choices: frontendDeps,
             default: []
         }, function (a) {
-            bowerDependencies = a.bowerDependencies;
+            npmDeps = npmDeps.concat(a.frontendDeps);
             cb();
         }.bind(this));
     },
@@ -90,42 +73,22 @@ module.exports = Base.extend({
         }
         this.spawnCommandSync('touch', ['README.md']);
         this.initNpmConfig();
-        this.initBowerConfig();
         cb();
     },
 
     install: function () {
 
         var p = this.p;
-        var npmDeps = ['express@4.13.3', 'body-parser@1.14.2', 'compression@1.6.1', 'request@2.69.0',
-            'mongoose@4.4.4', 'node-uuid@1.4.7', 'moment@2.12.0', 'multer@1.1.0'];
-        var npmDevDeps = ['bower@1.7.1', 'gulp@3.8.11',
-            'gulp-babel@6.1.2', 'babel-preset-es2015@6.6.0',
-            'gulp-concat@2.5.2', 'gulp-sourcemaps@1.5.0',
-            'gulp-uglify@1.1.0', 'gulp-postcss@6.1.0',
-            'autoprefixer@6.3.3', 'postcss-scss@0.1.6', 'postcss-pxtorem@3.3.1',
-            'gulp-sass@2.2.0', 'browser-sync@2.8.2'];
-        
+
         if (p && !('c' === p || 'f' === p)) {
             return;
         } else if (!p || 'f' === p) {
-            this.npmInstall(npmDeps, {'save': true}, function () {
-            });
-
-            this.npmInstall(npmDevDeps, {'saveDev': true}, function () {
-            });
-
-            this.bowerInstall(bowerDependencies, {'save': true}, function () {
-                this.spawnCommandSync('mv', ['-f', 'bower.json', 'bower_components', 'client']);
-            }.bind(this));
-        } else if ('c' === p) {
-            this.npmInstall(npmDevDeps, {'saveDev': true}, function () {
-            });
-
-            this.bowerInstall(bowerDependencies, {'save': true}, function () {
-                this.spawnCommandSync('mv', ['-f', 'bower.json', 'bower_components', 'client']);
-            }.bind(this));
+            var backendDeps = ['express@4.13.3', 'body-parser@1.14.2', 'compression@1.6.1', 'request@2.69.0',
+                'mongoose@4.4.4', 'node-uuid@1.4.7', 'moment@2.12.0', 'multer@1.1.0'];
+            npmDeps = npmDeps.concat(backendDeps);
         }
+        this.npmInstall(npmDeps, {'save': true});
+        this.npmInstall(npmDevDeps, {'saveDev': true});
     },
 
     done: function () {
--- app/templates/client/gulpfile.js
+++ app/templates/client/gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
-//const ngAnnotate = require('gulp-ng-annotate');
+const ngAnnotate = require('gulp-ng-annotate');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
@@ -14,74 +14,76 @@ const browserSync = require('browser-sync').create();
 
 function makeCss() {
 
-    var scssSrc = 'scss/app.scss',
-        cssDest = 'stylesheet/';
+    var scssSrc = 'client/scss/app.scss',
+        cssDest = 'client/stylesheet/';
 
     gulp.src(scssSrc)
-        .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
         .pipe(sass({
+            // includePaths: ['node_modules/materialize-css/sass', 'node_modules/susy/sass'],
             outputStyle: 'compressed'
         }).on('error', sass.logError))
-        .pipe(postcss([autoprefixer({browsers: ['last 10 versions']}),
-            pxtorem({
-                //rootValue: 75,
-                mediaQuery: true,
-                propWhiteList: []
-            })], {syntax: postcssScss}))
-        .pipe(sourcemaps.write('../map/', {
-            includeContent: false,
-            sourceRoot: '../scss/'
-        }))
+        .pipe(postcss([autoprefixer({browsers: ['last 10 versions']})
+            // pxtorem({
+            //     rootValue: 16,
+            //     mediaQuery: true,
+            //     propWhiteList: []
+            // })
+        ], {syntax: postcssScss}))
+        // .pipe(sourcemaps.write('../map/', {
+        //     includeContent: false,
+        //     sourceRoot: '../scss/'
+        // }))
         .pipe(gulp.dest(cssDest))
         .pipe(browserSync.reload({stream: true}));
 }
 
 function makeJs() {
 
-    //var jsSrc = ['js/app.js', 'js/ctrl/*.ctrl.js', 'js/svc/*.svc.js', 'js/drt/*.drt.js'],
-    var jsSrc = ['js/*.js'],
-        jsDest = 'script/';
+    var jsSrc = ['client/js/app.js', 'client/js/ctrl/*.ctrl.js', 'client/js/rsc/*.rsc.js', 'client/js/svc/*.svc.js', 'client/js/fty/*.fty.js', 'client/js/drt/*.drt.js'],
+        jsDest = 'client/script/';
 
     gulp.src(jsSrc)
-        .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
         .pipe(babel({
             presets: ['es2015']
         }))
-        //.pipe(ngAnnotate())
+        .pipe(ngAnnotate())
         .pipe(uglify())
-        .pipe(sourcemaps.write('../map/'))
+        // .pipe(sourcemaps.write('../map/'))
         .pipe(gulp.dest(jsDest))
         .pipe(browserSync.reload({stream: true}));
 }
 
-//function makeHtml(filePath) {
-//
-//    var src = filePath || ['js/partials/*.html', 'js/drt/*.html'],
-//        dest = 'html/partials/';
-//
-//    gulp.src(src)
-//        .pipe(gulp.dest(dest))
-//        .pipe(browserSync.reload({stream: true}));
-//}
+function makeTemplate(filePath) {
+
+    var src = filePath || ['client/js/drt/*.html'],
+        dest = 'client/template/';
+
+    gulp.src(src)
+        .pipe(gulp.dest(dest))
+        .pipe(browserSync.reload({stream: true}));
+}
 
 gulp.task('default', function () {
 
     browserSync.init({
-        startPath: 'index.html',
+        startPath: 'client/index.html',
         server: {
             baseDir: './'
         }
     });
 
-    gulp.watch(['js/**/*', 'scss/*', 'index.html'], function (event) {
+    gulp.watch(['client/js/**/*', 'client/scss/*', 'client/index.html'], function (event) {
+
         var filePath = event.path;
         if (filePath.lastIndexOf('.scss') !== -1) {
             makeCss();
         } else if (filePath.lastIndexOf('.js') !== -1) {
             makeJs();
-            //} else if (filePath.lastIndexOf('js') !== -1 && filePath.lastIndexOf('.html') !== -1) {
-            //    makeHtml(filePath);
+        } else if (filePath.lastIndexOf('.drt.html') !== -1) {
+            makeTemplate(filePath);
         } else {
             browserSync.reload();
         }
@@ -92,5 +94,5 @@ gulp.task('build', function () {
 
     makeCss();
     makeJs();
-    //makeHtml();
+    makeTemplate();
 });
