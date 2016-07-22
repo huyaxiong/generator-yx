8c799c8b23bb95aee9ccc2adef0f8d9de6b72385
hugh.hu <hugh.hu@fredfarid.com>
2016-07-22 16:12:37 +0800

    test

--- app/templates/server/model/User.js
+++ app/templates/server/model/User.js
@@ -5,6 +5,6 @@ var schema = mongoose.Schema({
     name: String,
     mobile: String
 });
-var User = mongoose.model('User', schema);
+var User = mongoose.model('User', schema, 'user');
 
 module.exports = User;
