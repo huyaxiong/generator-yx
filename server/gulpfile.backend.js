f1031705dfd238147803bbaf9980a7be19c26abf
hugh.hu <hugh.hu@fredfarid.com>
2016-10-28 11:54:02 +0800

    test

--- server/gulpfile.backend.js
+++ server/gulpfile.backend.js
@@ -1,12 +1,13 @@
+const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 
-function makeBackend() {
+function makeBackend(filePath, fileName) {
 
-    var backendSrc = 'backend/',
-        backendDest = 'server/';
+    var backendSrc = fileName ? 'backend/' + filePath + fileName : 'backend/**/*.js',
+        backendDest = fileName ? 'server/' + filePath : 'server/';
 
-    gulp.src(backendSrc + '**/*.js')
+    gulp.src(backendSrc)
         .pipe(babel({
             "presets": [
                 "es2017",
@@ -23,15 +24,25 @@ function makeBackend() {
             ]
         }))
         .pipe(gulp.dest(backendDest));
+}
 
-    gulp.src(backendSrc + '**/*.json')
-        .pipe(gulp.dest(backendDest));
+function makeBackendJSON() {
+
+    gulp.src('backend/**/*.json')
+        .pipe(gulp.dest('server/'));
 }
 
 gulp.task('backend', function () {
 
-    gulp.watch(['backend/**/*'], function (event) {
+    gulp.watch(['backend/**/*.js', 'backend/**/*.json'], function (event) {
 
-        makeBackend();
+        if (event.path.lastIndexOf('.json') !== -1) {
+            makeBackendJSON()
+        } else {
+            let filePath = event.path.slice(event.path.lastIndexOf('backend/') + 'backend/'.length, event.path.lastIndexOf('/') + 1);
+            let fileName = event.path.slice(event.path.lastIndexOf('/') + 1);
+            makeBackend(filePath, fileName);
+        }
     });
 });
+
