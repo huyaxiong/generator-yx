a8e72cf97982e8aa04c78f8635c8a864d8dbd999
胡亚雄 <hughyaxiong@gmail.com>
2018-08-23 11:41:44 +0800

    test

--- vue/templates/src/router.js
+++ vue/templates/src/router.js
@@ -1,18 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import User from './views/user.vue';
+import Home from './views/home.vue';
 
 
 Vue.use(VueRouter);
 
 const routes = [
     {
-        path: '/user', component: User, meta: {
+        path: '/home', component: Home, meta: {
+            title: '主页',
             keepAlive: true
         }
+    },
+    {
+        path: '*', redirect: '/home'
     }
 ];
 
 let router = new VueRouter({routes});
+
+router.beforeEach((to, from, next) => {
+
+    if (to.meta && to.meta.title) {
+        document.title = to.meta.title
+    }
+    next();
+});
 
 export default router;
---
+++ vue/templates/src/views/home.vue
@@ -0,0 +1,44 @@
+<template>
+
+<div id="home-view">
+</div>
+
+</template>
+
+
+<script>
+
+import axios from 'axios';
+
+
+export default {
+    data () {
+        return {}
+    },
+    methods: {
+        query() {
+
+        }
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    destroyed() {
+
+    }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+
+@import "home.scss";
+
+</style>
